b67fffc653a578ba32085cef8d7b8223588e1724
komiyamma <komiyama0629@gmail.com>
2023-04-20 16:53:11 +0900

    バージョンを1.0.0.0

--- src/obj/Debug/net6.0/HmChatGptInBrowser.AssemblyInfo.cs
+++ src/obj/Debug/net6.0/HmChatGptInBrowser.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HmChatGptInBrowser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HmChatGptInBrowser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HmChatGptInBrowser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/obj/Release/net6.0/HmChatGptInBrowser.AssemblyInfo.cs
+++ src/obj/Release/net6.0/HmChatGptInBrowser.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HmChatGptInBrowser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HmChatGptInBrowser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HmChatGptInBrowser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
